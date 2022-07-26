846a42b59d25f6e23b7454313c44bf04610b78e4
christhian brandão <christhianbrandao69@gmail.com>
2022-07-26 18:56:09 -0300

    exercios finalizados

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+const nomeUsuario = prompt("Qual o seu nome ?")
+const frutaUm = prompt("agora me pense nas suas três frutas favoritas\nFruta 1:")
+const  frutaDois = prompt("Fruta dois:")
+const  frutaTres = prompt("Fruta três:")
+
+const resutado = `Essas são as frutas favoritas de ${nomeUsuario}\n${frutaUm}\n${frutaDois}\n${frutaTres}`
+
+console.log(resutado)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,10 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const exercicioA = minhaString.trim()
+console.log(exercicioA)
+
+const exercicioB = minhaString.length
+console.log(exercicioB,exercicioA.length)
+
+const exercicioC = minhaString.replace("________","sticioso")
+console.log(exercicioC)
